d1aac9a453b69e16699b163dca07ce8eb98eeb6f
LUCASGOODPROGRAMMER <lucashubner2019@gmaiil.com>
2025-08-02 16:10:03 -0400

    config vite

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/quiz-base/',
+  base: '/quiz-basico/',
   plugins: [react()],
 })
